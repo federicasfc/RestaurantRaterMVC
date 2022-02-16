30389cfe4a377993a83e67303e7171e9e72f9a16
federicasfc <fesafeco@fakeemail.com>
2022-02-16 12:01:53 -0500

    Created RestaurantCreate model, built out Get/Post Create methods in controller, and started building out ResaurantCreateView

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -36,5 +36,40 @@ namespace RestaurantRaterMVC.Controllers
 
             return View(restaurants);
         }
+
+        //Get method
+        //Will return the Html form view that we'll use to create restaurants
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        //PostMethod for Create
+        //will take in the form data from the create view that EntityFramework will deserialize into the right format (C# RestaurantCreate model object)
+
+        public async Task<IActionResult> Create(RestaurantCreate model)
+        {
+            //if model invalid, return Create view with existing model and an error message added to with ViewBag, which can pass extra info to a View
+            if (!ModelState.IsValid)
+            {
+                ViewBag.errorMessage = ModelState.FirstOrDefault().Value.Errors.FirstOrDefault().ErrorMessage;
+                return View(model);
+
+            }
+            //if valid make new Restaurant(entity)
+            Restaurant restaurant = new Restaurant()
+            {
+                Name = model.Name,
+                Location = model.Location
+            };
+
+            //add to dbcontext and save to db
+            _context.Restaurants.Add(restaurant);
+            await _context.SaveChangesAsync();
+
+            //go back to Restaurant index and see new restaurant on list
+
+            return RedirectToAction(nameof(Index)); //When we do n-tier, will we go back to using a bool verification, or does something here render it unnecessary
+        }
     }
 }
